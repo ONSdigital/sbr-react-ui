05333f2874dbac9101ff3b044ec96be0eb9de241
Tom Cooling <Tom.Cooling@ons.gov.uk>
2017-07-06 10:53:38 +0100

    Add errorModal and seperate searchRefForm
    - Add errorModal which displays a message in a modal, uses provided callback to close itself
    - Add searchRefForm, includes all form elements (inputs/button) for searching for a ref

--- src/views/Search.js
+++ src/views/Search.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ControlLabel, PageHeader, FormGroup, FormControl, HelpBlock, Modal } from 'react-bootstrap';
-import Button from 'react-bootstrap-button-loader';
+import { PageHeader } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { refSearch, setQuery } from '../actions/ApiActions';
 import { SET_REF_QUERY } from '../constants/ApiConstants';
+import ErrorModal from '../components/ErrorModal';
+import SearchRefForm from '../components/SearchRefForm';
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       show: false,
+      errorMessage: '',
     };
     this.changeQuery = this.changeQuery.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.getValidationState = this.getValidationState.bind(this);
   }
   onSubmit(e) {
     e.preventDefault();
@@ -21,67 +25,44 @@ class Search extends React.Component {
     if (query.length > 5 && query.length < 13) {
       this.props.dispatch(refSearch(query));
     } else {
-      this.setState({ show: true });
+      this.setState({
+        show: true,
+        errorMessage: 'Please enter a valid VAT/PAYE/UBRN reference.',
+      });
     }
   }
   getValidationState() {
     const length = this.props.data.query.length;
     if (length > 12) return 'error';
     else if (length > 5) return 'success';
-    // else if (length > 5) return 'warning';
     else if (length > 0) return 'error';
+    return 'error';
+  }
+  closeModal() {
+    this.setState({ show: false });
   }
   changeQuery(evt) {
     this.props.dispatch(setQuery(SET_REF_QUERY, evt.target.value));
   }
   render() {
-    let close = () => this.setState({ show: false });
     return (
       <div>
         <PageHeader>
           Reference Search
           <small> by VAT/PAYE/UBRN reference</small>
         </PageHeader>
-        <form method="get">
-          <div style={{ width: '50%' }}>
-            <FormGroup
-              controlId="formBasicText"
-              validationState={this.getValidationState()}
-            >
-              <ControlLabel>Enter reference number:</ControlLabel>
-              <FormControl
-                type="text"
-                value={this.props.data.query}
-                placeholder="Enter ref to search..."
-                onChange={this.changeQuery}
-              />
-              <FormControl.Feedback />
-              <HelpBlock>Validation is based on string length.</HelpBlock>
-            </FormGroup>
-          </div>
-          <Button
-            bsStyle="primary"
-            type="submit"
-            id="searchButton"
-            loading={this.props.data.currentlySending}
-            disabled={this.props.data.currentlySending}
-            onClick={!this.props.data.currentlySending ? this.onSubmit : null}
-          >
-            {this.props.data.currentlySending ? '' : 'Search' }
-          </Button>
-        </form>
-        <Modal
+        <SearchRefForm
+          currentlySending={this.props.data.currentlySending}
+          onSubmit={this.onSubmit}
+          onChange={this.changeQuery}
+          value={this.props.data.query}
+          valid={this.getValidationState()}
+        />
+        <ErrorModal
           show={this.state.show}
-          onHide={close}
-          aria-labelledby="contained-modal-title"
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title">Contained Modal</Modal.Title>
-          </Modal.Header>
-          <Modal.Footer>
-            <Button onClick={close}>Close</Button>
-          </Modal.Footer>
-        </Modal>
+          message={this.state.errorMessage}
+          close={this.closeModal}
+        />
       </div>
     );
   }

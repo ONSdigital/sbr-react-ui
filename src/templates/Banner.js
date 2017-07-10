28b80732fa67aa93824677402d1fae881cccf84d
ONS-Tom <Tom.Cooling@ons.gov.uk>
2017-07-10 14:11:09 +0100

    Add banner displaying 'still in development' (#30)
    - Banner shows dynamically, only when ENV != prod

---
+++ src/templates/Banner.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Banner = function () {
+  const style = {
+    banner: {
+      width: '100vw',
+      backgroundColor: '#3B7A9E',
+      color: '#fff',
+      boxSizing: 'border-box',
+      display: 'block',
+      height: '40px',
+    },
+    bannerTag: {
+      paddingTop: '2px',
+      paddingRight: '7px',
+      paddingBottom: '3px',
+      paddingLeft: '7px',
+      textTransform: 'uppercase',
+      color: '#FFFFFF',
+      fontWeight: '700',
+      backgroundColor: '#323132',
+      boxSizing: 'border-box',
+    },
+    p: {
+      paddingTop: '10px',
+    },
+  };
+  return (
+    <div style={style.banner}>
+      <div style={style.banner}>
+        <div className="container">
+          <p style={style.p}>
+            <strong style={style.bannerTag}>
+              Experimental
+            </strong>&nbsp;
+            this release is still in development.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;

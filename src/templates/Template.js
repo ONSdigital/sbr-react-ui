614688cb057ebd1794ab6c1f74cb76433ec3d4cd
Tom Cooling <Tom.Cooling@ons.gov.uk>
2017-07-10 14:08:29 +0100

    Add banner displaying 'still in development'
    - Banner shows dynamically, only when ENV != prod

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -2,21 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Header from './Header';
 import NavBar from './NavBar';
+import Banner from './Banner';
+import config from '../config/constants';
+
+const { ENV } = config;
 
 const Template = function (props) {
+  const onProdEnv = (ENV === 'prod');
+  const banner = (onProdEnv) ? '' : (<Banner />);
   if (props.location.pathname === '/' || props.location.pathname === 'Login') {
     return (
-      <div className="container">
-        <Header />
-        {props.children}
+      <div>
+        {banner}
+        <div className="container">
+          <Header />
+          {props.children}
+        </div>
       </div>
     );
   }
   return (
-    <div className="container">
-      <Header />
-      <NavBar />
-      {props.children}
+    <div>
+      {banner}
+      <div className="container">
+        <Header />
+        <NavBar />
+        {props.children}
+      </div>
     </div>
   );
 };

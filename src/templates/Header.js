14d1f0284ad4e6dbd9be00d7471d06d2b8070e83
Tom Cooling <Tom.Cooling@ons.gov.uk>
2017-07-10 11:03:21 +0100

    Add config/constants for ENV var, update header
    - Fix lint errors
    - Use ENV from config/constants.js for header replace

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -1,47 +1,38 @@
 import React from 'react';
 import ONSLogo from '../resources/img/orglogo.svg';
+import config from '../config/constants';
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: '',
-    };
-  }
-  setHeader() {
-    // Default header is Example UI, unless on "/" or login page
-    let header = 'Local';
-    if (location.pathname === '/' || location.pathname === 'Login') {
-      header = 'Statistical Business Register';
-    }
-    return header;
-  }
-  render() {
-    return (
-      <div>
-        <header role="banner" id="global-header">
-          <div className="header-wrapper">
-            <div className="header-global" style={{ marginBottom: '20px' }}>
-              <div className="header-logo">
-                <a href="https://www.ons.gov.uk/" title="Go to the homepage" id="logo" className="content">
-                  <img
-                    src={ONSLogo}
-                    style={{ marginTop: '30px' }}
-                    width="240"
-                    alt="Office for National Statistics"
-                  />
-                </a>
-              </div>
-              <div className="banner-text">
-                <h1 style={{ marginTop: '25px' }}>{this.setHeader()}</h1>
-              </div>
+const { ENV } = config;
+
+const Header = function () {
+  // Once logged in, display ENV (local/dev/prod etc) in the header
+  const onLoginPage = (location.pathname === '/' || location.pathname === 'Login');
+  const header = (onLoginPage) ? 'Statistical Business Register' : ENV;
+  return (
+    <div>
+      <header role="banner" id="global-header">
+        <div className="header-wrapper">
+          <div className="header-global" style={{ marginBottom: '20px' }}>
+            <div className="header-logo">
+              <a href="https://www.ons.gov.uk/" title="Go to the homepage" id="logo" className="content">
+                <img
+                  src={ONSLogo}
+                  style={{ marginTop: '30px' }}
+                  width="240"
+                  alt="Office for National Statistics"
+                />
+              </a>
+            </div>
+            <div className="banner-text">
+              <h1 style={{ marginTop: '25px' }}>
+                {header}
+              </h1>
             </div>
           </div>
-        </header>
-      </div>
-    );
-  }
-}
+        </div>
+      </header>
+    </div>
+  );
+};
 
 export default Header;

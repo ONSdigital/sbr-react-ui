7f3c1f532c073778e53d14b411190a3ffd5e88df
ONS-Tom <Tom.Cooling@ons.gov.uk>
2017-07-11 12:55:43 +0100

    Refactor of table of enterprises in /Search (#33)
    - Use seperate EnterpriseResultsTable component
    - Move validation method into /utils
    - Add unit tests for validation method

--- src/views/Search.js
+++ src/views/Search.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { PageHeader, Button, Table } from 'react-bootstrap';
+import { PageHeader } from 'react-bootstrap';
 import { connect } from 'react-redux';
-import { browserHistory } from 'react-router';
 import { refSearch, setQuery } from '../actions/ApiActions';
 import { SET_REF_QUERY } from '../constants/ApiConstants';
 import ErrorModal from '../components/ErrorModal';
 import SearchRefForm from '../components/SearchRefForm';
+import EnterpriseResultsTable from '../components/EnterpriseResultsTable';
+import { validateRefSearch } from '../utils/validation';
 
 class Search extends React.Component {
   constructor(props) {
@@ -19,7 +20,6 @@ class Search extends React.Component {
     this.changeQuery = this.changeQuery.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
     this.closeModal = this.closeModal.bind(this);
-    this.getValidationState = this.getValidationState.bind(this);
   }
   componentWillReceiveProps(nextProps) {
     // The Redux action for the api request will set the errorMessage in the
@@ -49,13 +49,6 @@ class Search extends React.Component {
       });
     }
   }
-  getValidationState() {
-    const length = this.props.data.query.length;
-    if (length > 12) return 'error';
-    else if (length > 5) return 'success';
-    else if (length > 0) return 'error';
-    return 'error';
-  }
   closeModal() {
     this.setState({ show: false, errorMessage: '' });
   }
@@ -64,42 +57,8 @@ class Search extends React.Component {
     // presses 'back to search' on the Enterprise View page.
     this.props.dispatch(setQuery(SET_REF_QUERY, evt.target.value));
   }
-  displayEnterprises() {
-    const tableRows = this.props.data.results.map((enterprise, index) => {
-      return (
-        <tr>
-          <td>{enterprise.enterprise}</td>
-          <td>{enterprise.source}</td>
-          <td>{enterprise.name}</td>
-          <td>
-            <Button
-              onClick={() => browserHistory.push(`/Search/${enterprise.enterprise}/${index}`)}
-              bsStyle="info"
-            >
-                Go to record
-            </Button>
-          </td>
-        </tr>
-      );
-    });
-    return (
-      <Table style={{ width: '75%' }} striped bordered condensed hover>
-        <thead>
-          <tr>
-            <th style={{ width: '120px' }}>ID</th>
-            <th style={{ width: '80px' }}>Source</th>
-            <th >Name</th>
-            <th style={{ width: '120px' }}></th>
-          </tr>
-        </thead>
-        <tbody>
-          {tableRows}
-        </tbody>
-      </Table>
-    );
-  }
   render() {
-    const results = this.displayEnterprises();
+    const results = (<EnterpriseResultsTable results={this.props.data.results} />);
     const enterprises = (this.props.data.results.length > 1) ? results : <div></div>;
     return (
       <div>
@@ -112,7 +71,7 @@ class Search extends React.Component {
           onSubmit={this.onSubmit}
           onChange={this.changeQuery}
           value={this.props.data.query}
-          valid={this.getValidationState()}
+          valid={validateRefSearch(this.props.data.query.length)}
         />
         <ErrorModal
           show={this.state.show}

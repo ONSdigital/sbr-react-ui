0b4b52dd598b4ac30a7fc4aed86cb629840f019e
ONS-Tom <Tom.Cooling@ons.gov.uk>
2017-07-14 09:07:33 +0100

    Remove clustering from server/index.js (#40)
    - Remove clustering, leave in maxSockets
    - Remove cluster dependancy from package.json

--- server/index.js
+++ server/index.js
@@ -4,28 +4,11 @@
 /* eslint strict: "off" */
 /* eslint no-console: "off" */
 
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
 const app = require('./app');
 
 const PORT = process.env.PORT || 3001;
 
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
-
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i += 1) {
-    cluster.fork();
-  }
-
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-  });
-} else {
-  app.maxSockets = 500;
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-  });
-
-  console.log(`Worker ${process.pid} started`);
-}
+app.maxSockets = 500;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});

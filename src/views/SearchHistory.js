1baa37abc1748741c7fdc4ead3efef32525a19e7
Tom Cooling <Tom.Cooling@ons.gov.uk>
2017-07-14 13:38:29 +0100

    Resize badge in bootstrap-table of HTTP codes

--- src/views/SearchHistory.js
+++ src/views/SearchHistory.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { PageHeader, Label, Badge } from 'react-bootstrap';
+import { PageHeader, Label } from 'react-bootstrap';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import '../resources/css/react-bootstrap-table-all.min.css';
 import searchHistory from '../utils/addHistory';
 
 function columnClassNameFormat(fieldValue) {
   const condition = (fieldValue >= 200 && fieldValue < 400);
-  const colour = (condition) ? <Label bsStyle="success">{fieldValue}</Label> : <Label bsStyle="danger">{fieldValue}</Label>;
+  const style = { fontSize: '15px' };
+  const colour = (condition) ?
+    (<Label style={style} bsStyle="success">{fieldValue}</Label>) :
+    (<Label style={style} bsStyle="danger">{fieldValue}</Label>);
   return colour;
 }
 
 function countStatus(history, status) {
-  const count = history.filter(h => h.HTTPCode === status).length;
-  return count;
+  return history.filter(h => h.HTTPCode === status).length;
 }
 
 const SearchHistory = function () {
@@ -21,14 +23,19 @@ const SearchHistory = function () {
     labels: {
       marginRight: '20',
     },
+    h1: {
+      float: 'left',
+    },
   };
   return (
     <div>
       <PageHeader>Search History</PageHeader>
-      <Label bsStyle="success">200</Label>&nbsp;<Badge style={style.labels}>{countStatus(history, 200)}</Badge>
-      <Label bsStyle="danger">404</Label>&nbsp;<Badge style={style.labels}>{countStatus(history, 404)}</Badge>
-      <Label bsStyle="warning">500</Label>&nbsp;<Badge style={style.labels}>{countStatus(history, 500)}</Badge>
-      <br /><br />
+      <div style={{ clear: 'both' }}>
+        <h3 style={style.h1}><Label bsSize="large" bsStyle="success">200</Label>&nbsp;<Label style={style.labels}>{countStatus(history, 200)}</Label></h3>
+        <h3 style={style.h1}><Label bsStyle="danger">404</Label>&nbsp;<Label style={style.labels}>{countStatus(history, 404)}</Label></h3>
+        <h3 style={style.h1}><Label bsStyle="warning">500</Label>&nbsp;<Label style={style.labels}>{countStatus(history, 500)}</Label></h3>
+        <br /><br /><br /><br />
+      </div>
       <BootstrapTable
         striped
         data={history}

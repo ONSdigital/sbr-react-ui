8a7dadab52c4fb23966f744c59925b9a439ab6e5
Tom Cooling <Tom.Cooling@ons.gov.uk>
2017-07-21 10:24:27 +0100

    Fix display of Header, remove unused </nav>

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -11,33 +11,33 @@ const Header = function () {
   const header = (onLoginPage) ? 'Statistical Business Register' : ENV;
   let className1 = '';
   if (ie.version && ie.version <= 8) {
-    className1 = <img class="logo" src="https://cdn.ons.gov.uk/assets/images/ons-logo/v2/ons-logo.png" alt="Office for National Statistics" />;
+    className1 = <img className="logo" src="https://cdn.ons.gov.uk/assets/images/ons-logo/v2/ons-logo.png" alt="Office for National Statistics" />;
   } else {
-    className1 = <img class="logo" src="https://cdn.ons.gov.uk/assets/images/ons-logo/v2/ons-logo.svg" alt="Office for National Statistics" />;
+    className1 = <img className="logo" src="https://cdn.ons.gov.uk/assets/images/ons-logo/v2/ons-logo.svg" alt="Office for National Statistics" />;
   }
   return (
-    <div className="wrapper" style={{border:'solid'}}>
-    	<div className="header col-wrap">
-    		<div className="col col--lg-one-third col--md-one-third">
-    			<a href="/">
-             {className1}
-    			</a>
-    		</div>
-    		<div className="col col--lg-two-thirds col--md-two-thirds print--hide">&nbsp;</div>
-    		<div className="secondary-nav col col--lg-two-thirds col--md-two-thirds print--hide">
-    			<ul className="secondary-nav__list">
-    				<li className="secondary-nav__item">
-    					<a className="secondary-nav__link" href="/methodology">Methodology</a>
-    				</li>
-    				<li className="secondary-nav__item">
-    					<a className="secondary-nav__link" href="/aboutus">
-    						About
-    					</a>
-    				</li>
-    			</ul>
-    	</div>
+    <div className="wrapper">
+      <div className="header col-wrap">
+        <div className="col col--lg-one-third col--md-one-third">
+          <a href="/">
+            {className1}
+          </a>
+        </div>
+        <div className="col col--lg-two-thirds col--md-two-thirds print--hide">&nbsp;</div>
+        <div className="secondary-nav col col--lg-two-thirds col--md-two-thirds print--hide">
+          <ul className="secondary-nav__list">
+            <li className="secondary-nav__item">
+              <a className="secondary-nav__link" href="/methodology">Methodology</a>
+            </li>
+            <li className="secondary-nav__item">
+              <a className="secondary-nav__link" href="/aboutus">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  </div>
   );
 };
 

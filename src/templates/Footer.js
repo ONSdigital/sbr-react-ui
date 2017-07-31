784b330d05e59a6b8b528295e0fd2f4167ffb9ed
ONS-Anthony <ChiuA@MacBook-Pro.local>
2017-07-31 13:21:24 +0100

    Syntax fixes

--- src/templates/Footer.js
+++ src/templates/Footer.js
@@ -1,52 +1,52 @@
 import React from 'react';
-import { Link } from 'react-router';
 
 const Footer = function () {
-  const style = {
-    textDecoration: 'none',
-    margin: '20px',
-    color: 'black',
-  };
   return (
-    <footer className="print--hide" style={{
-      position: 'absolute', left: 0, right: 0, overflow: 'hidden'
-    }}>
-    <h2 className="visuallyhidden">Footer links</h2>
-        <div className="footer">
-            <div className="wrapper">
-                <nav>
-                    <div className="footer-nav col-wrap">
-                        <div className="col col--lg-one-third col--md-one-third">
-                            <h3 className="footer-nav__heading">Help</h3>
-                            <ul className="footer-nav__list">
-                                <li className="footer-nav__item">
-                                    <a href="/help">Accessibility</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="col col--lg-one-third col--md-one-third">
-                            <h3 className="footer-nav__heading">About SBR</h3>
-                            <ul className="footer-nav__list">
-                                <li className="footer-nav__item">
-                                    <a href="/aboutus/whatwedo">What is SBR</a>
-                                </li>
-                                <li className="footer-nav__item">
-                                    <a href="/aboutus/contactus">Contact us</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="col col--lg-one-third col--md-one-third">
-                            <h3 className="footer-nav__heading">Connect with us</h3>
-                            <ul className="footer-nav__list">
-                                <li className="footer-nav__item">
-                                    <a href="https://public.govdelivery.com/accounts/UKONS/subscribers/new" className="icon--hide">Email</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+    <footer
+      className="print--hide"
+      style={{
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        overflow: 'hidden',
+      }}
+    >
+      <h2 className="visuallyhidden">Footer links</h2>
+      <div className="footer">
+        <div className="wrapper">
+          <nav>
+            <div className="footer-nav col-wrap">
+              <div className="col col--lg-one-third col--md-one-third">
+                <h3 className="footer-nav__heading">Help</h3>
+                <ul className="footer-nav__list">
+                  <li className="footer-nav__item">
+                    <a href="/help">Accessibility</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col col--lg-one-third col--md-one-third">
+                <h3 className="footer-nav__heading">About SBR</h3>
+                <ul className="footer-nav__list">
+                  <li className="footer-nav__item">
+                    <a href="/aboutus/whatwedo">What is SBR</a>
+                  </li>
+                  <li className="footer-nav__item">
+                    <a href="/aboutus/contactus">Contact us</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col col--lg-one-third col--md-one-third">
+                <h3 className="footer-nav__heading">Connect with us</h3>
+                <ul className="footer-nav__list">
+                  <li className="footer-nav__item">
+                    <a href="https://public.govdelivery.com/accounts/UKONS/subscribers/new" className="icon--hide">Email</a>
+                  </li>
+                </ul>
+              </div>
             </div>
+          </nav>
         </div>
+      </div>
     </footer>
   );
 };

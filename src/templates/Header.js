691ef6f27d06cdbb09c4d7b1e13c69aac67b2f6d
ONS-Anthony <ChiuA@MacBook-Pro.local>
2017-08-17 10:40:00 +0100

    Logo banner

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -28,7 +28,9 @@ const Header = function (props) {
     <div className="wrapper">
       <div className="header col-wrap">
         <div className="col col--lg-one-third col--md-one-third">
-          <a onClick={() => browserHistory.push('/Home')} style={{ cursor: 'pointer' }}></a>
+          <a onClick={() => browserHistory.push('/Home')} style={{ cursor: 'pointer' }}>
+            <img className="logo" src="https://cdn.ons.gov.uk/assets/images/ons-logo/v2/ons-logo.svg" alt="Office for National Statistics" />;
+          </a>
         </div>
         <div className="col col--lg-two-thirds col--md-two-thirds print--hide">&nbsp;</div>
         {div}

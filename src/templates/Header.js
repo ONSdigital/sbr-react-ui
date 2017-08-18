fb16560e349929002c2fe8d5b0981f7ae9ab519b
ONS-Anthony <ChiuA@MacBook-Pro.local>
2017-08-18 11:04:07 +0100

    Removed semi colon from logo

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -29,7 +29,7 @@ const Header = function (props) {
       <div className="header col-wrap">
         <div className="col col--lg-one-third col--md-one-third">
           <a onClick={() => browserHistory.push('/Home')} style={{ cursor: 'pointer' }}>
-            <img className="logo" src="https://cdn.ons.gov.uk/assets/images/ons-logo/v2/ons-logo.svg" alt="Office for National Statistics" />;
+            <img className="logo" src="https://cdn.ons.gov.uk/assets/images/ons-logo/v2/ons-logo.svg" alt="Office for National Statistics" />
           </a>
         </div>
         <div className="col col--lg-two-thirds col--md-two-thirds print--hide">&nbsp;</div>

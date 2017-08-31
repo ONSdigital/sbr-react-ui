f5f3a94d0def85d500fb4b516cd399f8d5130736
Tom Cooling <Tom.Cooling@ons.gov.uk>
2017-08-31 10:42:49 +0100

    Add prototype code for TreeView using D3.js
    - Source: http://bl.ocks.org/robschmuecker/7880033
    - Show TreeView on UserDetails view (as an example)
    - Add d3 and jquery dependancy

--- src/views/UserDetails.js
+++ src/views/UserDetails.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import BreadCrumb from '../components/BreadCrumb';
+import * as d3 from 'd3';
+import flare from '../resources/flare.json';
+import $ from 'jquery';
+
+const json = {
+    "name": "flare",
+    "children": [{
+        "name": "analytics",
+        "children": [{
+            "name": "cluster",
+            "children": [{
+                "name": "AgglomerativeCluster",
+                "size": 3938
+            }, {
+                "name": "CommunityStructure",
+                "size": 3812
+            },
+          ]
+        }
+      ]
+    }
+  ]
+};
 
 const UserDetails = ({ username, role }) => {
   const items = [
@@ -14,6 +37,7 @@ const UserDetails = ({ username, role }) => {
         marginBottom={1}
         breadCrumbItems={items}
       />
+      <div style={{border:'solid'}} id="tree-container"></div>
       <div className="page-intro background--gallery">
         <div className="wrapper">
           <p className="page-intro__content">

b5acb49acf5b1aef8a66886b043f9814b7c6862c
Tom Cooling <Tom.Cooling@ons.gov.uk>
2017-09-14 17:55:27 +0100

    Add react-toggle for toggling collapsible tree mode

--- src/components/TreeView1.js
+++ src/components/TreeView1.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Tree from 'react-d3-tree';
+import { ButtonToolbar, Button } from 'react-bootstrap';
+import Toggle from 'react-toggle';
 
 function findAndReplace(object, value, replacevalue) {
   for (var x in object) {
@@ -11,16 +13,12 @@ function findAndReplace(object, value, replacevalue) {
       }
       if (object[value]) {
         object[replacevalue] = object[value] + ' ' + object['id'];
-        object['new_id'] = object['id'];
+        // id gets overwritten by react-d3-tree, so use newId instead
+        object['newId'] = object['id'];
         //delete object[value];
       }
     }
   }
-}
-
-function test(e) {
-  console.log('e is ',e)
-  console.log('testing...')
 }
 
 const TreeView1 = ({ enterpriseId, childrenJson }) => {
@@ -31,9 +29,35 @@ const TreeView1 = ({ enterpriseId, childrenJson }) => {
     children: childrenJson,
   }];
 
+  function handleClick(e) {
+    console.log(e.newId)
+    console.log(e.type)
+    console.log(collapsible)
+  }
+
+  let collapsible = false;
+
+  function handleToggle() {
+    collapsible = !collapsible
+    console.log(collapsible)
+  }
+
+  console.log(collapsible)
+
   return (
-    <div id="treeWrapper" style={{ width: '100%', height: '500px' }}>
-      <Tree data={json} orientation={'vertical'} onClick={e => test(e)} translate={translation} />
+    <div>
+      <div style={{ borderBottom: '2px solid', paddingBottom: '5px' }}>
+        <span className='label-text' style={{ fontSize: '20px', verticalAlign: 'top' }}>Toggle Collapsible &nbsp;&nbsp;</span>
+        <Toggle
+          collapsible={collapsible}
+          defaultChecked={false}
+          onChange={handleToggle}
+        />
+        <span className='label-text' style={{ fontSize: '16px', verticalAlign: 'top' }}>&nbsp;&nbsp;You can only go to a record when the collapsible toggle is off.</span>
+      </div>
+      <div id="treeWrapper" style={{ width: '100%', height: '500px' }}>
+        <Tree data={json} collapsible={false} orientation={'vertical'} onClick={e => handleClick(e)} translate={translation} />
+      </div>
     </div>
   );
 };

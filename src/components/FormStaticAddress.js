a147a561dd492f1b57880cfd901a9a0e402ab198
Tom Cooling <Tom.Cooling@ons.gov.uk>
2017-09-15 12:25:37 +0100

    Add dynamic address items
    - In FormStaticAddress, don't show empty address items

--- src/components/FormStaticAddress.js
+++ src/components/FormStaticAddress.js
@@ -2,19 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormGroup, FormControl, ControlLabel, Col } from 'react-bootstrap';
 
-
 const FormStaticAddress = ({ id, label, address1, address2, address3, address4, address5, postcode }) => {
   const url = `https://www.google.co.uk/maps/place/${postcode}`;
   const mapsLink = <a href={url} target="_blank">{postcode}</a>;
+  const addressArr = [address1, address2, address3, address4, address5, mapsLink];
+  // addressArr.filter(addressItem => addressItem !== '');
   return (
     <FormGroup controlId={id}>
-     <Col componentClass={ControlLabel} sm={6}>
-       {label}
-     </Col>
-     <Col sm={6}>
-       <FormControl.Static>{address1}<br/>{address2}<br/>{address3}<br/>{address4}<br/>{address5}<br/>{mapsLink}</FormControl.Static>
-     </Col>
-   </FormGroup>
+      <Col componentClass={ControlLabel} sm={6}>
+        {label}
+      </Col>
+      <Col sm={6}>
+        <FormControl.Static>
+          {
+            addressArr.map((addressItem) => {
+              if (addressItem !== '' && addressItem !== undefined) {
+                return (<div>{addressItem}<br /></div>);
+              }
+            })
+          }
+        </FormControl.Static>
+      </Col>
+    </FormGroup>
   );
 };
 
@@ -24,3 +33,17 @@ FormStaticAddress.propTypes = {
 };
 
 export default FormStaticAddress;
+
+
+// {/*}
+// {address1}
+// <br />
+// {address2}
+// <br />
+// {address3}
+// <br />
+// {address4}
+// <br />
+// {address5}
+// <br />
+// */}

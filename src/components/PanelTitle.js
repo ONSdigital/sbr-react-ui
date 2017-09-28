e96b0dab567f88360a64e60ad00b714efec13585
Tom Cooling <Tom.Cooling@ons.gov.uk>
2017-09-28 13:56:48 +0100

    Add dynamic react-tooltip for PanelTitle
    - When panelTitle overflows, show the tooltip
    - Use react-tooltip dependancy
    - Should use a bootstrap tooltip/overlay etc, however there is an issue with bootstrap styles for dynamic components

--- src/components/PanelTitle.js
+++ src/components/PanelTitle.js
@@ -1,20 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Glyphicon, ButtonToolbar, Button } from 'react-bootstrap';
+import { Glyphicon, Button, Row, Col } from 'react-bootstrap';
+import ReactTooltip from 'react-tooltip';
 
-const PanelTitle = function ({ name, id, toggle }) {
-  return (
-    <div style={{ height: '30px' }}>
-      <h3 style={{ marginTop: '0px', paddingBottom: '50px' }}>
-        <Glyphicon glyph="tower" />&nbsp;{name} <small>{id}</small>
-      </h3>
-      <ButtonToolbar className="pull-right" style={{ marginTop: '-92px' }}>
-        {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
-        <Button bsStyle="primary" onClick={() => toggle()}><Glyphicon glyph="tree-deciduous" />&nbsp;Toggle Tree View</Button>
-      </ButtonToolbar>
-    </div>
-  );
-};
+class PanelTitle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showTooltip: false,
+    };
+    this.mouseEnter = this.mouseEnter.bind(this);
+    this.mouseLeave = this.mouseLeave.bind(this);
+  }
+  mouseEnter() {
+    const a = document.getElementById('panel-title-data-tip');
+    if (a.offsetWidth < a.scrollWidth) {
+      this.setState({ showTooltip: true });
+    }
+  }
+  mouseLeave() {
+    this.setState({ showTooltip: false });
+  }
+  render() {
+    // Note: We should use a bootstrap tooltip/modal/overlay etc. for the title overflow,
+    // however there is an issue with dynamic bootstrap elements not inheriting css, so
+    // we use react-tooltip
+    const h3Style = {
+      margin: '0px',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+      whiteSpace: 'nowrap',
+      cursor: (this.state.showTooltip) ? 'pointer' : '',
+    };
+    const props = (this.state.showTooltip) ? {
+      'data-tip': true,
+      'data-for': 'tooltip',
+    } : '';
+    return (
+      <Row style={{ height: '30px', border: 'none' }}>
+        <Col lg={9} md={9} sm={8} xs={7} style={{ height: '30px', border: 'none' }}>
+          <h3 {...props} onMouseEnter={() => this.mouseEnter()} onMouseLeave={() => this.mouseLeave()} id="panel-title-data-tip" style={h3Style}>
+            <Glyphicon glyph="tower" />&nbsp;{this.props.name} <small>{this.props.id}</small>
+          </h3>
+        </Col>
+        <Col lg={3} md={3} sm={4} xs={5} style={{ height: '30px', border: 'none' }}>
+          <Button className="pull-right" bsSize="small" bsStyle="primary" onClick={() => this.props.toggle()}><Glyphicon glyph="tree-deciduous" />&nbsp;Toggle Tree View</Button>
+        </Col>
+        {this.state.showTooltip &&
+          <ReactTooltip id="tooltip" type="info">
+            <span>{this.props.name} - {this.props.id}</span>
+          </ReactTooltip>
+        }
+      </Row>
+    );
+  }
+}
 
 PanelTitle.propTypes = {
   name: PropTypes.string.isRequired,

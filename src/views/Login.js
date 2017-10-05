e51c07e580bd68bbdf982d87f0f07e6191749b1d
Tom Cooling <Tom.Cooling@ons.gov.uk>
2017-10-05 15:45:15 +0100

    Fix Login page to use refs rather than state

--- src/views/Login.js
+++ src/views/Login.js
@@ -11,24 +11,12 @@ import ONSLogo from '../resources/img/ons-symbol.svg';
 class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      username: '',
-      password: '',
-    };
-    this.changeUsername = this.changeUsername.bind(this);
-    this.changePassword = this.changePassword.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
   onSubmit(evt) {
     // http://stackoverflow.com/questions/39724481/cannot-post-error-react-js
     evt.preventDefault();
-    this.props.dispatch(login(this.state.username, this.state.password));
-  }
-  changeUsername(evt) {
-    this.setState({ username: evt.target.value });
-  }
-  changePassword(evt) {
-    this.setState({ password: evt.target.value });
+    this.props.dispatch(login(this.usernameInput.value, this.passwordInput.value));
   }
   render() {
     const spinner = (<Loader color="#FFFFFF" size="8px" margin="0px" />);
@@ -39,8 +27,8 @@ class Login extends React.Component {
             <form className="login-form">
               <img className="loginLogo" src={ONSLogo} />
               <h1>Statistical Business Register</h1>
-              <input type="text" placeholder="username" value={this.state.username} onChange={this.changeUsername}/>
-              <input type="password" placeholder="password" value={this.state.password} onChange={this.changePassword}/>
+              <input type="text" placeholder="username" ref={(ref) => (this.usernameInput = ref)} />
+              <input type="password" placeholder="password" ref={(ref) => (this.passwordInput = ref)} />
               <Button className="btn btn--primary btn--wide" bsStyle="primary" type="submit" id="loginButton" aria-label="Login button" onClick={!this.props.data.currentlySending ? this.onSubmit : null}>
                 {this.props.data.currentlySending ? spinner : 'Login'}
               </Button>

508cb163ca590645ad9229f65bdfeaf7b8fd2a5b
Tom Cooling <Tom.Cooling@ons.gov.uk>
2017-10-06 08:26:54 +0100

    Add LoginErrorMessage back into Login page

--- src/views/Login.js
+++ src/views/Login.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap-button-loader';
 import Loader from 'halogen/PulseLoader';
-import { Alert } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { login } from '../actions/LoginActions';
 import ErrorMessage from '../components/LoginErrorMessage';
@@ -25,18 +24,19 @@ class Login extends React.Component {
         <div className="login-page">
           <div className="form">
             <form className="login-form">
-              <img className="loginLogo" src={ONSLogo} />
+              <img className="loginLogo" role="presentation" src={ONSLogo} />
               <h1>Statistical Business Register</h1>
               <input type="text" placeholder="username" ref={(ref) => (this.usernameInput = ref)} />
               <input type="password" placeholder="password" ref={(ref) => (this.passwordInput = ref)} />
               <Button className="btn btn--primary btn--wide" bsStyle="primary" type="submit" id="loginButton" aria-label="Login button" onClick={!this.props.data.currentlySending ? this.onSubmit : null}>
                 {this.props.data.currentlySending ? spinner : 'Login'}
               </Button>
+              <ErrorMessage />
             </form>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 

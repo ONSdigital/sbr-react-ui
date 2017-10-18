af5dadb993a49dd01d39dde44fe73a40bb12e512
Tom Cooling <Tom.Cooling@ons.gov.uk>
2017-10-18 13:59:54 +0100

    Integrate Button with return to search on Data Views
    - Fix PAYEView bread crumb

--- src/components/PAYEPanel.js
+++ src/components/PAYEPanel.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button } from 'registers-react-library';
 import { Panel, Table, Form, Grid, Row, Col } from 'react-bootstrap';
 import { browserHistory } from 'react-router';
 import { formPayeJson } from '../utils/formJson';
@@ -127,9 +128,9 @@ const PAYEPanel = function ({ paye, showTreeView, toggleTreeView, goToView }) {
           {panelContent()}
         </Panel>
       </div>
-      <button style={{ marginTop: '20px' }} className="btn btn--primary margin-bottom-md--2" aria-label="Link back to Search page" onClick={() => browserHistory.push('/RefSearch')}>
-        Return to search
-      </button>
+      <div className="margin-bottom-md--2" style={{ marginTop: '20px' }}>
+        <Button id="returnToSearchButton" size="wide" text="Return to search" onClick={() => browserHistory.push('/RefSearch')} ariaLabel="Return to search Button" type="submit" />
+      </div>
     </div>
   );
 };

2700f3f804f9ab9c56c176522c01776144b6f94b
Tom Cooling <Tom.Cooling@ons.gov.uk>
2017-11-01 10:52:26 +0000

    Add code for using JSON sessions
    - Add consistent logging to RedisSessions.js

--- server/Sessions/JsonSession.js
+++ server/Sessions/JsonSession.js
@@ -1,22 +1,81 @@
+const uuidv4 = require('uuid/v4');
+const logger = require('../logger');
+const config = require('../config/sessions');
+
 class JsonSession {
   constructor() {
     this.name = 'json';
+    this.sessionExpire = config.SESSION_EXPIRE;
+    this.session = {};
   }
 
-  createSession() {
-
-  }
-
-  getApiKey() {
-
-  }
-
-  getSession() {
-
-  }
-
-  killSession() {
+  createSession(username, remoteAddress, key, role) {
+    logger.debug('Creating new JSON session');
     
+    return new Promise((resolve, reject) => {
+      const accessToken = uuidv4();
+      try {
+        this.session[accessToken] = {
+          key,
+          role,
+          username,
+          remoteAddress,
+        };
+        logger.debug('Create JSON session was successful');
+        resolve({ accessToken, role });
+      } catch (error) {
+        logger.error(`Unable to create JSON session: ${error}`);
+        reject({ error });
+      }
+    });
+  }
+
+  getApiKey(accessToken) {
+    logger.debug('Getting API Key from JSON session');
+
+    return new Promise((resolve, reject) => {
+      try {
+        const userSession = this.session[accessToken];
+        const username = userSession.accessToken;
+        const apiKey = userSession.key;
+        logger.debug('Get API Key from JSON session was successful');
+        resolve({ username, accessToken, apiKey });
+      } catch (error) {
+        logger.error(`Unable to get API key from JSON session: ${error}`);
+        reject({ error });
+      }
+    });
+  }
+
+  getSession(accessToken) {
+    logger.debug('Getting JSON session');
+    
+    return new Promise((resolve, reject) => {
+      try {
+        const userSession = this.session[accessToken];
+        const username = userSession.accessToken;
+        logger.debug('Get JSON session was successful');
+        resolve({ username, accessToken });
+      } catch (error) {
+        logger.error(`Unable to get JSON session: ${error}`);
+        reject({ error });
+      }
+    });
+  }
+
+  killSession(accessToken) {
+    logger.debug('Killing JSON session');
+    
+    return new Promise((resolve, reject) => {
+      try {
+        delete this.session[accessToken];
+        logger.debug('JSON session was successfully killed');
+        resolve();
+      } catch (error) {
+        logger.error(`Unable to kill JSON session: ${error}`);
+        reject({ error });
+      }
+    });
   }
 }
 
--- server/Sessions/RedisSession.js
+++ server/Sessions/RedisSession.js
@@ -12,6 +12,7 @@ class RedisSession {
 
   createSession(username, remoteAddress, key, role) {
     logger.debug('Creating new Redis session');
+
     return new Promise((resolve, reject) => {
       this.rs.create({
         app: this.rsapp,
@@ -19,52 +20,79 @@ class RedisSession {
         ip: remoteAddress,
         ttl: this.sessionExpire,
         d: { key },
-      }, (err, resp) => {
-        if (!err) resolve({ accessToken: resp.token, role });
-        reject({ error: err });
+      }, (error, resp) => {
+        if (!error) {
+          logger.debug('Create Redis session was successful');
+          resolve({ accessToken: resp.token, role });
+        } else {
+          logger.error(`Unable to create Redis session: ${error}`);
+          reject({ error });
+        }
       });
     });
   }
   
   getApiKey(accessToken) {
     logger.debug('Getting API Key from Redis session');
+
     return new Promise((resolve, reject) => {
       this.rs.get({
         app: this.rsapp,
         token: accessToken,
-      }, (err, resp) => {
-        if (err) reject();
-        // If the session has timed out, the response will be empty
-        if (Object.keys(resp).length === 0 && resp.constructor === Object) reject();
-        resolve({ username: resp.id, accessToken, apiKey: resp.d.key });
+      }, (error, resp) => {
+        if (error) {
+          logger.error(`Unable to get API key from Redis session: ${error}`);
+          reject({ error });
+        } else if (Object.keys(resp).length === 0 && resp.constructor === Object) {
+          // If the session has timed out, the response will be empty
+          logger.debug('Redis session has timed out');
+          reject({ error: 'Redis session has timed out' });
+        } else {
+          logger.debug('Get API Key from Redis session was successful');
+          resolve({ username: resp.id, accessToken, apiKey: resp.d.key });
+        }
       });
     });
   }
   
   getSession(accessToken) {
     logger.debug('Getting Redis session');
+
     return new Promise((resolve, reject) => {
       this.rs.get({
         app: this.rsapp,
         token: accessToken,
-      }, (err, resp) => {
-        if (err) reject();
-        // If the session has timed out, the response will be empty
-        if (Object.keys(resp).length === 0 && resp.constructor === Object) reject();
-        resolve({ username: resp.id, accessToken });
+      }, (error, resp) => {
+        if (error) {
+          logger.error(`Unable to get Redis session: ${error}`);
+          reject({ error });
+        } else if (Object.keys(resp).length === 0 && resp.constructor === Object) {
+          // If the session has timed out, the response will be empty
+          logger.debug('Redis session has timed out');
+          reject({ error: 'Redis session has timed out' });
+        } else {
+          logger.debug('Get Redis session was successful');
+          resolve({ username: resp.id, accessToken });
+        }
       });
     });
   }
   
   killSession(accessToken) {
     logger.debug('Killing Redis session');
+
     return new Promise((resolve, reject) => {
       this.rs.kill({
         app: this.rsapp,
         token: accessToken,
-      }, (err) => {
-        if (!err) resolve();
-        reject();
+      }, (error) => {
+        if (!error) {
+          logger.debug('Redis session was successfully killed');
+          resolve();
+        } else {
+          logger.error(`Unable to kill Redis session: ${error}`);
+          reject({ error });
+        }
       });
     });
   }
